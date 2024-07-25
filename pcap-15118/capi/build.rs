6cbaa756289642676a59fbbfff6cb988e29d6ae6
Ronan Le Martret <ronan.lemartret@iot.bzh>
2024-07-25 13:03:37 +0200

    Add link to libjson-c

--- pcap-15118/capi/build.rs
+++ pcap-15118/capi/build.rs
@@ -17,7 +17,7 @@ fn main() {
     println!("cargo:rustc-link-arg=-lgnutls");
     println!("cargo:rustc-link-arg=-lnettle");
     println!("cargo:rustc-link-arg=-liso15118");
-    println!("cargo:rustc-link-arg=-llibjson-c");
+    println!("cargo:rustc-link-arg=-ljson-c");
 
     // check pkgconfig dependencies
     system_deps::Config::new().probe().unwrap();
